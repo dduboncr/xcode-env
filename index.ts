c43252a11a7b04f628b9e76310a662deea99c2a5
dduboncr <diego.dubon@proximitycr.com>
2022-11-15 21:31:31 +0000

    added gitignore

--- index.ts
+++ index.ts
@@ -1,1 +1,3 @@
-console.log("hello world")
+export const sum = (a, b) => {
+  return a + b;
+};
---
+++ tests/index.spec.ts
@@ -0,0 +1,5 @@
+import * as helpers from '../';
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(helpers.sum(1, 2)).toBe(3);
+});
